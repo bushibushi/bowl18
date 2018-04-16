20c03207aa07baa21340912798e52dd333c1af27
Enzo Guérin <enzoguerin@orange.fr>
2018-04-16 17:26:28 +0200

    Created a function that extracts a box arround a cell mask

--- extract_boxes.py
+++ extract_boxes.py
@@ -1,7 +1,31 @@
 import os
-from PIL import Image
+from PIL import Image, ImageDraw
 
+def frameCell(mask_path):
+    image = Image.open(os.path.join(os.getcwd(), mask_path))
+    px = image.load()
+    xmin = image.width
+    xmax = 0
+    ymin = image.height
+    ymax = 0
 
-image = Image.open(os.path.join(os.getcwd(), '../stage1_train/00071198d059ba7f5914a526d124d28e6d010c92466da21d4a04cd5413362552/masks/0f5a3252d05ecdf453bdd5e6ad5322c454d8ec2d13ef0f0bf45a6f6db45b5639.png'))
+    for i in range(0, image.width):
+        for j in range(0, image.height):
 
+            if(px[i,j] != 0):
+                if(i > xmax):
+                    xmax = i
+                if(i < xmin):
+                    xmin = i
+                if(j > ymax):
+                    ymax = j
+                if(j < ymin):
+                    ymin = j
+
+    return xmin, xmax, ymin, ymax
+
+xmin, xmax, ymin, ymax = frameCell('../stage1_train/0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9/masks/0adbf56cd182f784ca681396edc8b847b888b34762d48168c7812c79d145aa07.png')
+image = Image.open(os.path.join(os.getcwd(), '../stage1_train/0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9/masks/0adbf56cd182f784ca681396edc8b847b888b34762d48168c7812c79d145aa07.png'))
+imageFrame = ImageDraw.Draw(image)
+imageFrame.rectangle([xmin, ymin, xmax, ymax], outline=(128))
 image.show()
